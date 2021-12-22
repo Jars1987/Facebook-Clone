3060a8b1fb046ce864f70d16adcd2b619c9b0eb8
José Santos <josesantos@MacBook-Pro-de-Jose.local>
2021-12-22 12:03:25 +0000

    firebase back to version 9.6.1 for now might bring back the bug, need to check No secret first

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,9 +11,11 @@ export default NextAuth({
     // ...add more providers here
   ],
   session: {
-    jwt: true,
+    strategy: 'jwt',
   },
   jwt: {
     secret: process.env.JWT_SECRET,
+    maxAge: 30 * 24 * 60 * 60, // 30 days
   },
+  secret: process.env.JWT_SECRET,
 });
